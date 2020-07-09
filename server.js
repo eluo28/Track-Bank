a5adb656e1177bd825557ec11a4acf3681d5a295
Edwin Luo <edwinluo28@gmail.com>
2020-07-09 10:55:17 -0400

    v 1.1

--- server.js
+++ server.js
@@ -23,13 +23,14 @@ app.use('/api/users',require('./routes/api/users'));
 app.use('/api/auth',require('./routes/api/auth'));
 
 //serve static assets if in prod
-if(proccess.env.NODE_ENV==='production'){
+if(process.env.NODE_ENV==='production'){
     app.use(express.static('client/build'))
 
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    }
-    )}
+    })
+
+}
 
 const port = process.env.PORT || 5000;
 
