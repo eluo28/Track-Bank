dac20b4d21504da8771901ef948fe72bff5690ce
Edwin Luo <edwinluo28@gmail.com>
2020-07-09 11:12:37 -0400

    git ignore update

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use('/api/auth',require('./routes/api/auth'));
 
 //serve static assets if in prod
 if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
+    app.use(express.static('client/build'));
 
     app.get('*',(req,res)=>{
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
